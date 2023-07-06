0df07837ecc68089026495671c7545ff093961ae
RangaRaj Dhanuskodi <rangarajdhanushkodi@gmail.com>
2023-07-06 19:45:35 +0100

    Wrote code to check if the add method throws exception for negative numbers and throws the values for the multiple negative number

--- src/com/string/calc/main/StringCalculator.java
+++ src/com/string/calc/main/StringCalculator.java
@@ -3,6 +3,7 @@ package com.string.calc.main;
 public class StringCalculator {
 	static int total_value=0;
 	static String delimiter=",";
+	static String negativeString = "";
 	public static int add(String string) {
 		
 		if(string.equals("")) {
@@ -23,7 +24,16 @@ public class StringCalculator {
 	}
 	private static int sum(String[] numbers) {
 		for(String number : numbers) {
+			if(Integer.parseInt(number) < 0){
+        		if(negativeString.equals("")) {
+        			negativeString = number;}
+        		else {
+        			negativeString += ("," + number);}
+        	}
 			total_value=total_value+Integer.parseInt(number);
+		}
+		if(!negativeString.equals("")){
+			throw new IllegalArgumentException("Negatives not allowed: " + negativeString);
 		}
 		return total_value;
 	}
