8f44379f3a6c8ecf7aed2badba98a1e00979c023
RangaRaj Dhanuskodi <rangarajdhanushkodi@gmail.com>
2023-07-06 15:47:41 +0100

    created git repo and written first test case as per the TDD model

---
+++ src/com/string/calc/test/StringCalculatorTest.java
@@ -0,0 +1,18 @@
+package com.string.calc.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.string.calc.main.Calculator;
+
+class StringCalculatorTest {
+	//Task-1
+	//For example "" or "1" or "1,2" as inputs. (for an empty string it will return 0)
+	//Writing first test case to return 0 for empty string
+	@Test
+	void testingEmptyString() {
+		assertEquals(0,StringCalculator.add(""));
+	}
+
+}
